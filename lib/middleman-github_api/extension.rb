455495f001f960f43133bd24ebbd08c921295486
Masafumi Yokoyama <myokoym@gmail.com>
2015-11-14 17:19:20 +0900

    Register an extension and change directory

---
+++ lib/middleman-github_api/extension.rb
@@ -0,0 +1,29 @@
+module Middleman::GithubApi
+  class Extension < ::Middleman::Extension
+    option :my_option, 'default', 'An example option'
+
+    def initialize(app, options_hash={}, &block)
+      # Call super to build options from the options_hash
+      super
+
+      # Require libraries only when activated
+      # require 'necessary/library'
+
+      # set up your extension
+      # puts options.my_option
+    end
+
+    def after_configuration
+      # Do something
+    end
+
+    # A Sitemap Manipulator
+    # def manipulate_resource_list(resources)
+    # end
+
+    # module do
+    #   def a_helper
+    #   end
+    # end
+  end
+end
